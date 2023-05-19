7ae81a79dbce037fd19d3afb0175a90a0f37865a
ginguyenn <ginguyenn@gmail.com>
2023-05-19 02:04:13 +0200

    Navbar erstellen

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,20 @@
 <template>
-  <h1>Das is eine Navbar</h1>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">My Person App</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+
+          <router-link class="nav-link" to="/">Home</router-link>
+          <router-link class="nav-link" to="/about">About</router-link>
+          <router-link class="nav-link" to="/persons">Persons</router-link>
+        </div>
+      </div>
+    </div>
+  </nav>
 </template>
 
 <script>
